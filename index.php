1f3017c744249b8bc7c0242bd6d282c5ab96ba0d
EADG <EADG@wolke7.net>
2013-11-19 02:15:20 +0100

    inital push
    Should work, tonight it does.

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<h2> get all free parking lots, provided by the city of dresden: http://www.dresden.de/freie-parkplaetze </h2>
+
+<h2>Get them all in json via: ../json.php</h2>
+via: ../json.php?view=array get the var_dump of the data
+
+</html>
